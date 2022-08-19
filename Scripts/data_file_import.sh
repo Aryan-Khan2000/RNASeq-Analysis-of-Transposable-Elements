3ba0d43605a51828d01f7422a9acb3f4d2c4a929
Aryan-Khan2000 <93936591+Aryan-Khan2000@users.noreply.github.com>
2022-08-19 12:50:49 +0100

    Delete Scripts directory

--- Scripts/data_file_import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/017/SRR12842717/SRR12842717.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/018/SRR12842718/SRR12842718.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/019/SRR12842719/SRR12842719.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/020/SRR12842720/SRR12842720.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/021/SRR12842721/SRR12842721.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/022/SRR12842722/SRR12842722.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/023/SRR12842723/SRR12842723.fastq.gz
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/024/SRR12842724/SRR12842724.fastq.gz
